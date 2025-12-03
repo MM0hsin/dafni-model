dcc8a99bad2dabf0ebe89bfb96c2191ff3010796
Muhammad Mohsin <muhammad.mohsin@stfc.ac.uk>
2025-12-03 09:37:44 +0000

    push changes

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,15 @@
 FROM python:3.12
+RUN pip install --upgrade pip
+WORKDIR /app
 
 COPY requirements.txt .
 RUN pip install --no-cache-dir --compile -r requirements.txt
 
-COPY /aiida-mlip ./
+COPY ./aiida-mlip /app/aiida-mlip/
+
+RUN mkdir -p /app/outputs/
 
 RUN verdi presto
-RUN verdi code create core.code.installed --config janus_code.yml
+RUN verdi code create core.code.installed --config /app/aiida-mlip/janus_code.yml
 
+CMD ["python3", "/app/aiida-mlip/aiida-mlip-workgraph.py"] 
--- aiida-mlip/aiida-mlip-workgraph.py
+++ aiida-mlip/aiida-mlip-workgraph.py
@@ -1,33 +1,69 @@
 from aiida import load_profile
-from aiida.orm import StructureData
-from ase.build import bulk
+from aiida.orm import StructureData, Str, load_code, Dict, SinglefileData
 from ase.io import read
 from aiida_mlip.data.model import ModelData
-from aiida.orm import load_code
-from aiida.orm import Str
 from aiida.plugins import CalculationFactory
 from aiida.engine import run_get_node
+from pathlib import Path
 
-load_profile()
+import os
+import shutil
+import yaml
 
-structure = StructureData(ase=bulk("NaCl", "rocksalt", 5.63))
-uri = "https://github.com/stfc/janus-core/raw/main/tests/models/mace_mp_small.model"
-model = ModelData.from_uri(uri, architecture="mace_mp", cache_dir="mlips")
+OUTPUT_FOLDER= Path("/app/outputs")
 
-code = load_code("janus@localhost")
 
-inputs = {
-    "metadata": {"options": {"resources": {"num_machines": 1}}},
-    "code": code,
-    "arch": model.architecture,
-    "struct": structure,
-    "model": model,
-    "device": Str("cpu"),
-}
+def run_singlepoint(structure_path: Path):
+    load_profile()
 
-singlepointCalc = CalculationFactory("mlip.sp")
+    structure = StructureData(ase=read(structure_path))
+    uri = "https://github.com/stfc/janus-core/raw/main/tests/models/mace_mp_small.model"
+    model = ModelData.from_uri(uri, architecture="mace_mp", cache_dir="mlips")
 
-result, node = run_get_node(singlepointCalc, inputs)
+    code = load_code("janus@localhost")
 
-print(result)
-print(node)
+    inputs = {
+        "metadata": {"options": {"resources": {"num_machines": 1}}},
+        "code": code,
+        "arch": model.architecture,
+        "struct": structure,
+        "model": model,
+        "device": Str("cpu"),
+    }
+
+    singlepointCalc = CalculationFactory("mlip.sp")
+
+    result, node = run_get_node(singlepointCalc, inputs)
+
+    return result, node
+
+def calculation_output_file(results_dict: Dict, xyz_output: SinglefileData):
+
+    with xyz_output.open(mode='rb') as source:
+        with open(f"{OUTPUT_FOLDER}/structure.txt", mode='wb') as target:
+            shutil.copyfileobj(source, target)
+    
+    with open(f"{OUTPUT_FOLDER}/results_dict.txt", mode='w') as target:
+        yaml.dump(results_dict.get_dict(), target)
+
+
+def main():
+
+    inputs = os.listdir('aiida-mlip/inputs')
+    print("inputs", inputs)
+
+    input_structure = 'aiida-mlip/inputs/' + inputs[0]
+
+    result, node = run_singlepoint(Path(input_structure))
+
+    print(result['results_dict'])
+    print(result['xyz_output'])
+
+    calculation_output_file(result['results_dict'], result['xyz_output'])
+
+
+if __name__ == "__main__":
+    main()
+
+    
+
